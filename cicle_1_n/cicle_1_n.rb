185deafaa59f0f60d22dd9112828125afd5a1547
Bryan <bryan.guerrero@michelada.io>
2022-05-30 16:42:34 -0500

    1/n cycle kata

---
+++ cicle_1_n/cicle_1_n.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+# https://www.codewars.com/kata/5a057ec846d843c81a0000ad
+
+def cycle(div)
+  char = (1.0/div).to_s.split(".")[1]
+  len = 2
+
+  loop do
+    break if char[0...len] == char[len...len*2]
+
+    if len >= char.length
+      len = -1
+      break
+    end
+
+    len += 1
+  end
+
+  len
+end
