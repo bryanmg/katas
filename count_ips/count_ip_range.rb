3800651db2ec87c9b047f5aaf4f530fef6fefe71
Bryan <bryan.guerrero@michelada.io>
2022-06-06 19:25:21 -0500

    Kata 18

---
+++ count_ips/count_ip_range.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def ipsBetween(init_range, end_range)
+  return false unless has_valid_format?(init_range, end_range)
+  
+  factors = []
+  end_range = end_range.split(".").map(&:to_i)
+  init_range = init_range.split(".").map(&:to_i)
+
+  init_range.each_with_index {|num, index| factors << (end_range[index] - num)}
+
+  units = factors.pop
+  remanent = factors.reject{|x| x==0 }.size
+  exponential = 255 * (factors.reject{|x| x==0 }&.inject(&:*) || 0)
+  exponential + units + remanent
+end
+
+def has_valid_format?(initial, final)
+  ipv4_regex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
+  initial.match?(ipv4_regex) && final.match?(ipv4_regex)
+end
