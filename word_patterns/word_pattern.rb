6ab3433a8240c98dec7daa1c00bf51bd89470182
Bryan <bryan.guerrero@michelada.io>
2022-06-13 23:24:10 -0500

    Kata 19

---
+++ word_patterns/word_pattern.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# kata: https://www.codewars.com/kata/562846dd1f77ab7c00000033
+
+def word_pattern(pattern_str, sentence)
+  pattern = pattern_str.split('')
+  words = sentence.split(' ')
+
+  return false if pattern.uniq.size != words.uniq.size
+
+  text = sentence
+  pattern.uniq.each_with_index do |ptt, idx|
+    text = text.gsub(/#{words.uniq[idx]}/, ptt)
+  end
+  text.gsub!(/ /, '')
+
+  return text == pattern_str
+end
---
+++ word_patterns/word_pattern_test.rb
@@ -0,0 +1,17 @@
+
+require_relative 'word_pattern'
+require 'test/unit'
+
+class WordPatterbTest < Test::Unit::TestCase
+  def test_cases
+    assert_equal word_pattern('abab', 'truck car truck car'), true
+    assert_equal word_pattern('aaaa', 'dog dog dog dog'), true
+    assert_equal word_pattern('abab', 'apple banana banana apple'), false
+    assert_equal word_pattern('aaaa', 'cat cat dog cat'), false
+    #dojo
+    assert_equal word_pattern('xxxx', 'cat cat cat cat'), true
+    assert_equal word_pattern('ccc', 'cat dog cat'), false
+    assert_equal word_pattern('abcdf', 'cat men dog tigger woman'), true
+    assert_equal word_pattern('ttt', 'cat cat cat'), true
+  end
+end
