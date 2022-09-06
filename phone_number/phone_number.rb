11caec8bb03d2014accfcc532089a07e596e7537
Bryan <bryan.guerrero@michelada.io>
2022-09-05 22:09:15 -0500

    Add validation to phone kata

--- phone_number/phone_number.rb
+++ phone_number/phone_number.rb
@@ -1,3 +1,14 @@
-def create_phone_number(numbers)
-  "(#{numbers[0..2].join}) #{numbers[3..5].join}-#{numbers[6..9].join}"
+# frozen_string_literal: true
+
+class LengthError < StandardError; end
+class FormatError < StandardError; end
+
+class PhoneNumber
+  def self.format(numbers)
+    raise LengthError if numbers.length != 10
+
+    raise FormatError if numbers.join.match?(/[^0-9]/)
+
+    "(#{numbers[0..2].join}) #{numbers[3..5].join}-#{numbers[6..9].join}"
+  end
 end
--- phone_number/phone_number_test.rb
+++ phone_number/phone_number_test.rb
@@ -5,8 +5,21 @@ require 'test/unit'
 
 class PhoneNumberTest < Test::Unit::TestCase
   def test_cases
-    assert_equal create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890"
-    assert_equal create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890"
-    assert_equal create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111"
+    assert_equal PhoneNumber.format([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123) 456-7890'
+    assert_equal PhoneNumber.format([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123) 456-7890'
+    assert_equal PhoneNumber.format([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), '(111) 111-1111'
+
+    # with bad size
+    assert_raises LengthError do
+      assert_equal PhoneNumber.format([1, 1, 1]), '(111) 111-1111'
+    end
+    assert_raises LengthError do
+      assert_equal PhoneNumber.format([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), '(111) 111-1111'
+    end
+
+    # with wrong characters
+    assert_raises FormatError do
+      assert_equal PhoneNumber.format([1, 1, 'B', 1, 'A', 1, 1, 1, 'C', 1]), '(111) 111-1111'
+    end
   end
 end
